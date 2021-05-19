5529f7ec027409b60abd309a855cd2f8f9f7e519
yuc125 <71300761+yuc125@users.noreply.github.com>
2021-05-18 20:46:16 -0400

    Update vmspikerates.m

--- @vmspikerates/vmspikerates.m
+++ @vmspikerates/vmspikerates.m
@@ -53,17 +53,44 @@ end
 
 function obj = createObject(Args,varargin)
 
-% example object
-dlist = nptDir;
-% get entries in directory
-dnum = size(dlist,1);
+% look for spiketrains file
+    dlist = nptDir('spiketrain.mat');
+    % get entries in directory list
+    dnum = size(dlist,1);
+    % load rplparallel object
+    % the constructor function for rplparallel will automatically 
+    % change to the session directory to attempt to load the saved
+    % object
+    rp = rplparallel('auto',varargin{:});
+ 
+    % continue only if both required files are present
+    if( (dnum>0) && (~isempty(rp)) )
+    % load the spiketrains
+    l = load(dlist(1).name);
+    % we are going to compute spike counts using the histcounts
+    % function there are 3 marker times per trial: 1) cue onset, 
+    % 2) cue offset, and 3) reward/time-out. But since we are
+    % only interested in the counts between the 2nd and 3rd
+    % markers, we will use only those values to create the
+    % histcounts bins by extracting the 2nd and 3rd columns
+    % transposing the values, and then converting the values to a
+    % column vector
+    timestamps = rp.data.timeStamps(:,2:3)';
+    hbins = reshape(timestamps,[],1);
+    % count spikes between marker times
+    % need to divide timestamps by 1000 as rplparallel timestamps are
+    % seconds while spiketrain timestamps are in milliseconds
+    hcounts = histcounts(l.timestamps/1000,hbins');
+    % reshape hcounts into 2 rows so we can discard the counts 
+    % between the 3rd marker of 1 trial and the 2nd marker of the
+    % next trial
+    hcounts2 = reshape([hcounts 0],2,[]);
+    % we will need the corresponding time intervals to compute
+    % spike rate    we will then store the remaining spike counts in
+    % the object
+    data.rates = hcounts2(1,:)' ./ diff(timestamps)';
+    data.avgrate = mean(data.rates);
 
-% check if the right conditions were met to create object
-if(dnum>0)
-	% these are object specific fields
-	data.dlist = dlist;
-	% set index to keep track of which data goes with which directory
-	data.setIndex = [0; dnum];
 	
 	% create nptdata so we can inherit from it
 	data.numSets = 1;    
